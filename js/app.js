d6fa9afa576e933ae2e2f9e253bd95925f305e56
awwadsaeed <saeedawwad450@gmail.com>
2021-04-05 16:06:38 +0300

    making the table header

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@
 
 
 let workingHhours = ['6 am', '7 am', '8 am', '9 am', '10am', '11am', '12pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
-let myObject =[];
+let myObject = [];
 //create the object template
-function Shop(name,minCustomer,maxCustomer,avgCookies){
-    this.locationName=name;
-    this.minCustomer=minCustomer;
-    this.maxCustomer=maxCustomer;
-    this.avgCookies=avgCookies;
-    this.randCustomers=[];
+function Shop(name, minCustomer, maxCustomer, avgCookies) {
+    this.locationName = name;
+    this.minCustomer = minCustomer;
+    this.maxCustomer = maxCustomer;
+    this.avgCookies = avgCookies;
+    this.randCustomers = [];
     this.cookiesSoldPerHour = [];
     myObject.push(this);
 }
 //calculate random customer number
-Shop.prototype.randomNumber= function() {
-    let arr=[];
+Shop.prototype.randomNumber = function () {
+    let arr = [];
     for (let i = 0; i < workingHhours.length; i++) {
-       this.randCustomers.push(Math.floor(Math.random()*(this.maxCustomer-this.minCustomer+1)+this.minCustomer));
+        this.randCustomers.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer));
     };
     return arr;
 }
@@ -25,41 +25,65 @@ Shop.prototype.randomNumber= function() {
 // calculate avg cookies sold per hour and store in array
 Shop.prototype.cookiesSold = function () {
 
-  
-    for (let i = 0; i <14; i++) {
-        this.cookiesSoldPerHour[i]= Math.floor(this.avgCookies * this.randCustomers[i]);
+
+    for (let i = 0; i < 14; i++) {
+        this.cookiesSoldPerHour[i] = Math.floor(this.avgCookies * this.randCustomers[i]);
     }
 }
 
 
 //rendering output
-Shop.prototype.renderOutput=function (hours){
+let mainElemnt = document.getElementById('mainElement');
+Shop.prototype.renderOutput = function (hours) {
     hours = workingHhours;
-    let mainElemnt = document.getElementById('mainElement');
     let name = document.createElement('h2');
     mainElemnt.appendChild(name);
     name.textContent = this.locationName;
     let ulElement = document.createElement('ul');
     mainElemnt.appendChild(ulElement);
-    for(let i=0;i<workingHhours.length;i++){
+    for (let i = 0; i < workingHhours.length; i++) {
         let liElement = document.createElement('li');
-        liElement.textContent= `${hours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
+        liElement.textContent = `${hours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
         ulElement.appendChild(liElement);
     }
 }
 
-//creating objects and rendering them
+let table = document.createElement('table');
+mainElemnt.appendChild(table);
+
+
+//make the table header
+function tableHeaders() {
+    let tableHeader = document.createElement('tr');
+    table.appendChild(tableHeader);
+    for (let i = 0; i <= workingHhours.length; i++) {
+        let headerCell = document.createElement('th');
+        table.appendChild(headerCell);
+        if (i == 0) {
+            headerCell.textContent = '------';
+        } else {
+            headerCell.textContent = `${workingHhours[i - 1]}`;
+        }
+    }
+}
+tableHeaders();
+
+
 let seattle = new Shop('seattle',23,65,6.3);
 let tokyo = new Shop('tokyo',3,24,1.2);
 let dubai = new Shop('dubai',11,38,3.7);
 let paris = new Shop('paris',20,38,2.3);
 let lima = new Shop('lima',2,16,4.6);
 
-for(let i=0;i<myObject.length;i++){
-    myObject[i].randomNumber();
-    myObject[i].cookiesSold();
-    myObject[i].renderOutput();
-}
+
+
+//creating objects and rendering them
+
+// for(let i=0;i<myObject.length;i++){
+//     myObject[i].randomNumber();
+//     myObject[i].cookiesSold();
+//     myObject[i].renderOutput();
+// }
 
 // let seattle = {
 //     locationName:'seattle',
